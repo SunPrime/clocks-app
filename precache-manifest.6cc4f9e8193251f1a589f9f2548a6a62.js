b43a9fc2fb98b22cdf3d6d5e12a0f135e3a9f8e3
sunprime <sunprime@ukr.net>
2019-09-25 15:59:52 +0300

    Updates

---
+++ precache-manifest.6cc4f9e8193251f1a589f9f2548a6a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93fd8e2d94c74cb2b332b2db6a187a7",
+    "url": "/clocks-app/index.html"
+  },
+  {
+    "revision": "a0b058beb6e0566ad024",
+    "url": "/clocks-app/static/css/2.1b53b9c3.chunk.css"
+  },
+  {
+    "revision": "8509325b3e8466a90e7d",
+    "url": "/clocks-app/static/css/main.b0982933.chunk.css"
+  },
+  {
+    "revision": "a0b058beb6e0566ad024",
+    "url": "/clocks-app/static/js/2.ec8491a8.chunk.js"
+  },
+  {
+    "revision": "8509325b3e8466a90e7d",
+    "url": "/clocks-app/static/js/main.a7312793.chunk.js"
+  },
+  {
+    "revision": "60992b4dc05ac1fae09d",
+    "url": "/clocks-app/static/js/runtime-main.9c116315.js"
+  }
+]);
